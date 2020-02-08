fbb631e990a95a6778a174c726a38d211d4c6d46
Juan Jesús <jjpulidos@unal.edu.co>
2020-02-08 13:50:06 -0500

    :sparkles: Added Invoice Generator and Fixed typo data field in Invoices

--- Analytics/Data_Generator.py
+++ Analytics/Data_Generator.py
@@ -37,29 +37,68 @@ dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 #         'name': name
 #     }
 # )
-#
-# print("PutItem succeeded:")
-# print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
 
 
 # Adding a row in Product
-Product = dynamodb.Table(tableName["Product"])
-email = "multifunctionallab@gmail.com"
-name = "Tomate Cherry"
-description = "Libra de Tomate Proveniente del Olimpo"
-price = decimal.Decimal('12000.45')
-expired_date = datetime.datetime.now().strftime("%x %X")
+# Product = dynamodb.Table(tableName["Product"])
+# email = "multifunctionallab@gmail.com"
+# name = "Tomate Cherry"
+# description = "Libra de Tomate Proveniente del Olimpo"
+# price = decimal.Decimal('12000.45')
+# expired_date = datetime.datetime.now().strftime("%x %X")
+# #
+# response = Product.put_item(
+#    Item={
+#         'id': str(bson.objectid.ObjectId()),
+#         'tenant_id': "ec059827-3fdd-4ed7-86c2-0a6ec60b6f3b",
+#         'name': name,
+#         'description': description,
+#         'price': price,
+#         'expired_date': expired_date
+#     }
+# )
+
+
+# # Adding a row in Customer
+# Customer = dynamodb.Table(tableName["Customer"])
+# tenant_id = "ec059827-3fdd-4ed7-86c2-0a6ec60b6f3b"
+# first_name = "Juan"
+# last_name = "Pulido"
 #
-response = Product.put_item(
+# response = Customer.put_item(
+#    Item={
+#         'id': str(bson.objectid.ObjectId()),
+#         'tenant_id': tenant_id,
+#         'first_name': first_name,
+#         'last_name': last_name
+#     }
+# )
+
+
+# Adding a row in Invoices
+Invoices = dynamodb.Table(tableName["Invoices"])
+tenant_id = "ec059827-3fdd-4ed7-86c2-0a6ec60b6f3b"
+customer_id = "5e3eff138cefb30c13bd541a"
+doc_date = datetime.datetime.now().strftime("%x %X")
+doc_number = "1"
+total_discount = 0
+total_tax = 0
+total_value = 24001
+
+response = Invoices.put_item(
    Item={
         'id': str(bson.objectid.ObjectId()),
-        'tenant_id': "ec059827-3fdd-4ed7-86c2-0a6ec60b6f3b",
-        'name': name,
-        'description': description,
-        'price': price,
-        'expired_date': expired_date
+        'tenant_id': tenant_id,
+        'customer_id': customer_id,
+        'doc_date': doc_date,
+        'doc_number': doc_number,
+        'total_discount': total_discount,
+        'total_tax': total_tax,
+        'total_value': total_value
     }
 )
-#
+
+# #########################
 print("PutItem succeeded:")
 print(json.dumps(response, indent=4, cls=DecimalEncoder))
--- Analytics/Read_DB.py
+++ Analytics/Read_DB.py
@@ -25,6 +25,10 @@ class DecimalEncoder(json.JSONEncoder):
 
 dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
 
+
+
+
+
 # Tenant = dynamodb.Table(tableName["Tenant"])
 # response = Tenant.scan(
 #     FilterExpression=Key('email').eq("multifunctionallab@gmail.com")
@@ -33,10 +37,36 @@ dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
 # for i in response['Items']:
 #     print(i['name'], ":", i['email'], ":", i['id'])
 
-Product = dynamodb.Table(tableName["Product"])
-response = Product.scan(
-    FilterExpression=Key('name').eq("Tomate Cherry")
+
+
+
+# Product = dynamodb.Table(tableName["Product"])
+# response = Product.scan(
+#     FilterExpression=Key('name').eq("Tomate Cherry")
+# )
+#
+# for i in response['Items']:
+#     print(i['name'], ":", i['description'], ":", i['id'])
+
+
+
+
+# Customer = dynamodb.Table(tableName["Customer"])
+#
+# response = Customer.scan(
+#     FilterExpression=Key('last_name').eq("Pulido")
+# )
+#
+# for i in response['Items']:
+#     print(i['tenant_id'], ":", i['first_name'], ":", i['last_name'])
+#
+#
+
+Invoices = dynamodb.Table(tableName["Invoices"])
+
+response = Invoices.scan(
+    FilterExpression=Key('doc_number').eq("1")
 )
 
 for i in response['Items']:
-    print(i['name'], ":", i['description'], ":", i['id'])
+    print(i['tenant_id'], ":", i['customer_id'], ":", i['total_value'], ":", i["doc_date"])
